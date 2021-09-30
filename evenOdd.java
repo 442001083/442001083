aec90bc6f3e4776b82486f8d990b9f4628b71732
442001083 <91378343+442001083@users.noreply.github.com>
2021-09-30 12:25:30 +0300

    Create evenOdd.java

---
+++ evenOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class evenOdd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    int deg = scan.nextInt();
+      if  (deg%2==0)
+    System.out.println("even");
+    
+   else 
+    System.out.println("add");   
+       
+    }
+}
+
