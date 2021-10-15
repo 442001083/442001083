083df2d9ea18e3810eab6cf2ac0a6c7290025f35
442001083 <91378343+442001083@users.noreply.github.com>
2021-10-15 03:46:01 +0300

    Update evenOdd.java

--- evenOdd.java
+++ evenOdd.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-public class evenOdd {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-    int deg = scan.nextInt();
-      if  (deg%2==0)
-    System.out.println("even");
-    
-   else 
-    System.out.println("add");   
-       
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner xyz = new Scanner(System.in);
+		System.out.println("enter a number ");
+		int num = xyz.nextInt();
+		if(num % 2==0)
+	System.out.println("Even");
+	
+	else
+	System.out.println("Add");
+	}
 }
 
